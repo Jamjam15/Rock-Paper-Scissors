7919b9db5870b38bd6510d4532a5f8d7c9121620
Jamjam15 <marianojamil15@gmail.com>
2023-11-02 14:25:47 +0800

    Create HTML and JavaScript files and add a new function
    This commit includes the following changes:
    - Added an HTML file (index.html).
    - Added a JavaScript file (script.js).
    - Implemented a new function in script.js.
    
    This functionality will [briefly describe what the function does].

---
+++ javascript.js
@@ -0,0 +1,17 @@
+// Your game is going to play against the computer, so begin with a function called getComputerChoice
+
+// that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+
+function getComputerChoice(playerSelection, computerSelection) {
+    console.log();
+}
+
+const playerSelection = prompt("Rock, Paper, or Scissors?");
+const computerSelection = getComputerChoice;
+console.log(getComputerChoice(playerSelection, computerSelection));
+
+// Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
+
+function game() {
+    
+}
