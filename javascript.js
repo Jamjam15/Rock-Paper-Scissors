534433f2f44ad1373ef45bf99840efdd6f3d0b7e
Jamjam15 <marianojamil15@gmail.com>
2023-11-02 15:03:53 +0800

    Added playerSelection function to validate and return the player's choice in Rock, Paper, Scissors game.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
-// Your game is going to play against the computer, so begin with a function called getComputerChoice
+// Your game is going to play against the computer
 
-// that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
-function getComputerChoice(playerSelection, computerSelection) {
+
+// The computer will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+function getComputerChoice() {
 
     // This generates random number between 1 to 3
     const randomNum = Math.floor(Math.random() * 3) + 1;
@@ -18,10 +19,36 @@ function getComputerChoice(playerSelection, computerSelection) {
     }
 }
 
+// Rock, Paper, Scissors
+function playerSelection() {
+    if (randomString == "Rock") {
+        return "Rock";
+    } else if (randomString == "Paper") {
+        return "Paper";
+    } else if (randomString == "Scissors") {
+        return "Scissors";
+    } else {
+        return "Invalid Input";
+    }
+}
+
+// Player's choice of Rock Paper Scissosrs
+const randomString = prompt("Rock, Paper, or Sicssors");
+
+function singleRound(playerSelection, computerSelection) {
+    if (playerSelection == Rock) {
+
+    }
+}
+
+
+// random selection for the Computer
 const computerSelection = getComputerChoice;
+
+
+
 console.log(getComputerChoice(playerSelection, computerSelection));
 
-const playerSelection = prompt();
 
 // Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
 
